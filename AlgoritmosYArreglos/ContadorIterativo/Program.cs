fe797a5dd27a5557b686f69d45bd0b860c988049
fernandofilipuzzi.utn <fernandofilipuzzi-utn@gmail.com>
2025-05-29 12:20:36 -0300

    iterativo y recursivo

---
+++ AlgoritmosYArreglos/ContadorIterativo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ContadorIterativo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //imprimir 1 2 3 4 5 6 7 8 9
+            for (int contador = 0; contador < 10; contador++)
+            {
+                Console.Write($"{contador,3} ");
+            }
+        }
+    }
+}
---
+++ AlgoritmosYArreglos/ContadorRecursivo/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ContadorRecursivo
+{
+    internal class Program
+    {
+
+        public static void ImprimirContador(int contador)
+        {
+            if (contador < 10) //condición de corte
+            {
+                Console.Write($"{contador,3} ");
+                ImprimirContador(contador + 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //imprimir 1 2 3 4 5 6 7 8 9
+            int contador = 0;
+            ImprimirContador(contador);
+        }
+    }
+}
